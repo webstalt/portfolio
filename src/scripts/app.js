8979043cc074212697f6dba2ba2731d34e25d84f
Mariya <mvmariya@bk.ru>
2017-12-27 15:46:20 +0300

    menu animateв

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,12 +2,10 @@ const slider = require('./common/slider');
 // const map = require('./common/map');
 const svg4everybody = require('./common/svg4everybody');
 const $ = require('jquery');
-// const flip = require('./common/flip');
 
 
 // const threejs = require('./common/three.min.js');
 // const webGL = require('./common/webgl');
-// flip();
 slider();
 // map();
 $();
@@ -15,31 +13,34 @@ svg4everybody();
 // threejs();
 // webgl();
 
-document.addEventListener('DOMContentLoaded', function() {
-       
-    console.log('dom is ready');
-});
 
-const container = document.querySelector('.index__wrap');
-const flipButton = document.querySelector('.index__flip-button');
-const flipBack = document.querySelector('.index__button-back');
+// flip
+if (document.querySelector('.index__wrap') !== null) {
 
-(function() {
-            
-        flipButton.addEventListener('click', e => {
+    const container = document.querySelector('.index__wrap');
+    const flipButton = document.querySelector('.index__flip-button');
+    const flipBack = document.querySelector('.index__button-back');
+
+    flipButton.addEventListener('click', e => {
+    e.preventDefault();
+    container.classList.toggle("index__wrap_rotate");
+    });
+
+    flipBack.addEventListener('click', e => {
+    e.preventDefault();
+    container.classList.toggle("index__wrap_rotate");
+    });
+}
+
+
+// menu 
+if (document.querySelector('.hero__menu-icon') !== null) {
+    
+    const menuButton = document.querySelector('.hero__menu-icon');
+    const menu = document.querySelector('.hero__menu');
+                
+    menuButton.addEventListener('click', e => {
         e.preventDefault();
-        container.classList.toggle("index__wrap_rotate");
-        });
-   
-        flipBack.addEventListener('click', e => {
-        e.preventDefault();
-        container.classList.toggle("index__wrap_rotate");
-        });
-})();
-
-// const burgerInit = document.querySelector('hamburger');
-
-// burgerInit.addEventListener('click', e => {
-//     e.preventDefault();
-//     burgerInit.classList.toggle('hero__menu_active'); 
-// });
+        menu.classList.toggle('hero__menu_active'); 
+    });
+}
