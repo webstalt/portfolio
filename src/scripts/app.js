0ba05789b5828c8884a47d59606706d2d4a1d4c5
Mariya <mvmariya@bk.ru>
2018-01-04 20:39:50 +0300

    anchor settings

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -64,12 +64,7 @@ if (document.querySelector('.articles__item') !== null){
             .getBoundingClientRect().top > clientCenterY) {            
             leftHeadings[i].classList.remove('blog__headings-item_active');
             } 
-        } 
-
-        for (var i = 0; i < articleItem.length; i++) {
-            if (articleItem[i].getBoundingClientRect().top > -15 && articleItem[i]
-            .getBoundingClientRect().top <= clientCenterY) {
-            // const activeAttr = articleItem[i].getAttribute('data-scroll-to');
+            else {
             leftHeadings[i].classList.add('blog__headings-item_active');
             } 
         }    
@@ -86,61 +81,84 @@ if (document.querySelector('.articles__item') !== null){
 //for tablets
     const sidebarTrigger = document.querySelector('.blog__sidebar-trigger');
     sidebarTrigger.onclick = function(e){
-        sidebar.classList.toggle('blog__sidebar_visible');
-        blogContent.classList.toggle('blog__content_short');
+        document.querySelector('.blog__sidebar').classList.toggle('blog__sidebar_visible');
+        document.querySelector('.blog__content').classList.toggle('blog__content_short');
     }
-
-    //anchors
-    (function() {  
-           var smoothScroll = function (anchor, duration) {
-               var startLocation = window.pageYOffset;
-               var endLocation = anchor.offsetTop;
-               var distance = endLocation - startLocation;
-               var increments = distance/(duration/16);
-               var stopAnimation;
-   
-               var animateScroll = function () {
-                   window.scrollBy(0, increments);
-                   stopAnimation();
-               };
-   
-               if ( increments >= 0 ) {
-                   stopAnimation = function () {
-                       var travelled = window.pageYOffset;
-                       if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-                           clearInterval(runAnimation);
-                       }
-                   };
-               }
-               else {
-                   stopAnimation = function () {
-                       var travelled = window.pageYOffset;
-                       if ( travelled <= (endLocation || 0) ) {
-                           clearInterval(runAnimation);
-                       }
-                   };
-               }   
-               var runAnimation = setInterval(animateScroll, 16);
-            };
-           var scrollToggle = document.querySelectorAll('.blog__headings-link');
-   
-           [].forEach.call(scrollToggle, function (toggle) {
-   
-               toggle.addEventListener('click', function(e) {
-   
-                   e.preventDefault();   
-                   var dataID = toggle.getAttribute('href');
-                   var dataTarget = document.querySelector(dataID);
-                   var dataSpeed = toggle.getAttribute('data-speed');
-   
-                   if (dataTarget) {
-                       smoothScroll(dataTarget, dataSpeed || 500);
-                   }
-   
-               }, false);   
-           });
-    })();
 }
+    //anchors
+
+if (document.querySelector('.blog__headings-link') !== null){ 
+    (function() {  
+            // Function to animate the scroll
+            var smoothScroll = function (anchor, duration) {
+    
+                // Calculate how far and how fast to scroll
+                var startLocation = window.pageYOffset;
+                var endLocation = anchor.offsetTop;
+                var distance = endLocation - startLocation;
+                var increments = distance/(duration/16);
+                var stopAnimation;
+    
+                // Scroll the page by an increment, and check if it's time to stop
+                var animateScroll = function () {
+                    window.scrollBy(0, increments);
+                    stopAnimation();
+                };
+    
+                // If scrolling down
+                if ( increments >= 0 ) {
+                    // Stop animation when you reach the anchor OR the bottom of the page
+                    stopAnimation = function () {
+                        var travelled = window.pageYOffset;
+                        if ( (travelled >= (endLocation - increments)) ) 
+                        {
+                            clearInterval(runAnimation);
+                        }
+                    };
+                }
+                // If scrolling up
+                else {
+                    // Stop animation when you reach the anchor OR the top of the page
+                    stopAnimation = function () {
+                        var travelled = window.pageYOffset;
+                        if ( travelled <= (endLocation || 0) ) {
+                            clearInterval(runAnimation);
+                        }
+                    };
+                }
+    
+                // Loop the animation function
+                var runAnimation = setInterval(animateScroll, 16);
+           
+            };
+    
+            // Define smooth scroll links
+            var scrollToggle = document.querySelectorAll('.blog__headings-link');
+
+        [].forEach.call(scrollToggle, function (toggle) {
+
+            // When the smooth scroll link is clicked
+            toggle.addEventListener('click', function(e) {
+
+                // Prevent the default link behavior
+                e.preventDefault();
+
+                // Get anchor link and calculate distance from the top
+                var dataID = toggle.getAttribute('href');
+                var dataTarget = document.querySelector(dataID);
+                var dataSpeed = toggle.getAttribute('data-speed');
+
+                // If the anchor exists
+                if (dataTarget) {
+                    // Scroll to the anchor
+                    smoothScroll(dataTarget, dataSpeed || 500);
+                }
+
+            }, false);
+
+        });
+ })();
+};
 
 if (document.querySelector('.map') !== null){
     map();
